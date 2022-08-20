da337ef5d1995c6843c1c51b061c6ab5aa378c64
“mbuimbogo” <peter.mbogo@student.moringaschool.com>
2022-08-20 23:31:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array,str){
+    return array.filter(name=>name.toLowerCase() === str.toLowerCase());
+}
+
+function fuzzyMatch(array, stri) {
+    return array.filter(name => name.startsWith(stri))
+}
+
+function matchName(array, str) {
+    return array.filter(obj => obj.name === str)
+}
